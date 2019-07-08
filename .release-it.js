6f6358d3f9bc9290ad1e522ba1bea890617ebeb0
ryokik <ryokik@users.noreply.github.com>
2019-07-08 10:14:32 +0900

    re-package
    Fix ci flow

--- .release-it.js
+++ .release-it.js
@@ -1,6 +1,5 @@
 const package = require('./package.json');
-const version = 'v' + package.version;
-const artifact = 'archives/' + package.name + '-' + version + '.zip';
+const artifact = 'archives/' + package.name + '-v' + package.version + '.zip';
 
 module.exports = {
     git: {
@@ -8,12 +7,13 @@ module.exports = {
         tag: false,
         requireUpstream: false,
         push: false,
+        tagName: package.version
     },
     github: {
         release: true,
-        releaseName: version,
+        releaseName: package.version,
         assets: [artifact],
-        releaseNotes: `node -e "console.log(require('fs').readFileSync('CHANGELOG.md', 'utf8').split(/(?:\\n)?\\d+\\.\\d+\\.\\d+(?:(?:-|\\+).*)?\\n-+\\n(?:\\n)?/g)[1])"`,
+        releaseNotes: `node -pe "require('fs').readFileSync('CHANGELOG.md', 'utf8').split(/(?:\\n)?\\d+\\.\\d+\\.\\d+(?:(?:-|\\+).*)?\\n-+\\n(?:\\n)?/g)[1]"`,
         tokenRef: 'PEGASUS_CI_GITHUB_TOKEN'
     },
     npm: false
