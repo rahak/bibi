71a4229e192c123a30caac07699d793d69587f8f
Satoru MATSUSHIMA (℠) <soarstre@gmail.com>
2019-07-09 15:20:34 +0900

    Make it difficult to save content data.

--- dev-bib/i/extensions/lunascape/index.js
+++ dev-bib/i/extensions/lunascape/index.js
@@ -84,6 +84,32 @@ Bibi.x({
         this.error = console.error || function(EM) { throw EM; };
     }
 
+    const VPs = ['-webkit-', '-moz-', '-ms-', ''], unaccessibilize = (Item) => {
+        if(this['select-elements'] == 'prevent') {
+            VPs.forEach(Prefix => {
+                ['user-select', 'user-drag'].forEach(Property => {
+                    Item.Body.style[Prefix + Property] = 'none';
+                });
+            });
+        }
+        if(this['save-images'] == 'prevent') {
+            sML.forEach(Item.Body.querySelectorAll('img, svg, image'))(Img => {
+                VPs.forEach(Prefix => {
+                    ['user-select', 'user-drag'].forEach(Property => {
+                        Img.style[Prefix + Property] = 'none';
+                    });
+                    if(O.Touch) Img.style[Prefix + 'pointer-events'] = 'none';
+                });
+                Img.draggable = false;
+                Img.addEventListener('contextmenu', O.preventDefault);
+            });
+        }
+        if(this['use-contextmenu'] == 'prevent') {
+            Item.contentDocument.addEventListener('contextmenu', O.preventDefault);
+        }
+    };
+    unaccessibilize(O), E.bind('bibi:postprocessed-item', unaccessibilize);
+
 })(function() {
 })(function() {
 });
